3624760dc8a62c944b5fd766b19cc7278f87b7ef
Han <hanky_chung@hotmail.com>
2019-05-08 11:10:21 +0800

    init

---
+++ README.md
@@ -0,0 +1,49 @@
+## description
+a tool for copying object/array in a real deep way
+
+## usage
+```
+npm i deepcopy_han -D
+
+import deepCopy from 'deepcopy_han'
+
+let a = {
+  age: 1,
+  jobs: {
+    first: "Fir"
+  },
+  lang: [
+    {
+      name: 'usa'
+    },
+    {
+      name: 'cn'
+    }
+  ],
+  arr: [
+    [
+      {
+        value: '1'
+      }
+    ],
+    [
+      {
+        value: '2'
+      }
+    ],
+  ]
+};
+
+let dc = deepCopy(a)
+```
+
+## params
+_example: deepCopy(input)_
+
+**_input_**
+
+type: `object`, `array`
+
+desc: things you want to deep copy
+
+return: things that have been copied and have no any relationship with the source
---
+++ index.js
@@ -0,0 +1,45 @@
+function isRaw(e) {
+  if (typeof e !== 'object') return true
+  return false
+}
+
+function isArr(e) {
+  if (e instanceof Array) return true
+  return false
+}
+
+function deepCopy(e) {
+  let obj = {}
+  for (let k in e) {
+    let v = e[k]
+    if (isRaw(v)) {
+      obj[k] = v
+    } else if (isArr(v)) {
+      obj[k] = copyArr(v)
+    } else {
+      obj[k] = deepCopy(v)
+    }
+  }
+  return obj
+}
+
+function copyArr(e) {
+  let arr = []
+  e.forEach(item => {
+    if (isRaw(item)) {
+      arr.push(item)
+    } else if (isArr(item)) {
+      arr.push(copyArr(item))
+    } else {
+      arr.push(deepCopy(item))
+    }
+  })
+  return arr
+}
+
+export default e => {
+  if (isRaw(e)) return e
+  if (isArr(e)) return copyArr(e)
+  if (typeof (e) === 'object') return deepCopy(e)
+  throw 'function is not supported'
+}
