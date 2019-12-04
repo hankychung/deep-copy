e0f74a42a796836dc5fdde350fa1030bb84602a1
Han <hanky_chung@hotmail.com>
2019-12-04 11:17:55 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,45 +1,46 @@
 function isRaw(e) {
-  if (typeof e !== 'object') return true
-  return false
+  if (Object.prototype.toString.call(e) !== "[object Object]") return true;
+  return false;
 }
 
 function isArr(e) {
-  if (e instanceof Array) return true
-  return false
+  if (e instanceof Array) return true;
+  return false;
 }
 
 function deepCopy(e) {
-  let obj = {}
+  let obj = {};
   for (let k in e) {
-    let v = e[k]
+    let v = e[k];
     if (isRaw(v)) {
-      obj[k] = v
+      obj[k] = v;
     } else if (isArr(v)) {
-      obj[k] = copyArr(v)
+      obj[k] = copyArr(v);
     } else {
-      obj[k] = deepCopy(v)
+      obj[k] = deepCopy(v);
     }
   }
-  return obj
+  return obj;
 }
 
 function copyArr(e) {
-  let arr = []
+  let arr = [];
   e.forEach(item => {
     if (isRaw(item)) {
-      arr.push(item)
+      arr.push(item);
     } else if (isArr(item)) {
-      arr.push(copyArr(item))
+      arr.push(copyArr(item));
     } else {
-      arr.push(deepCopy(item))
+      arr.push(deepCopy(item));
     }
-  })
-  return arr
+  });
+  return arr;
 }
 
 export default e => {
-  if (typeof (e) === 'function') throw 'FUNCTION is not supported to be copy source in @hankychung/deep-copy'
-  if (isRaw(e)) return e
-  if (isArr(e)) return copyArr(e)
-  if (typeof (e) === 'object') return deepCopy(e)
-}
+  if (typeof e === "function")
+    throw "FUNCTION is not supported to be copy source in @hankychung/deep-copy";
+  if (isRaw(e)) return e;
+  if (isArr(e)) return copyArr(e);
+  if (typeof e === "object") return deepCopy(e);
+};
